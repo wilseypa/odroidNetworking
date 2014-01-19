27c423539f4fd57891a0b6abefbb203143894bc6
ecarver <carver@ecarver.tk>
2014-01-19 14:28:20 -0500

    Finished initial coding of latency testing script

--- run_lat_tests.pl
+++ run_lat_tests.pl
@@ -5,13 +5,13 @@ use warnings;
 
 my $mode = shift || 'server';
 my $size = 2;
-my $output_file = 'lat_tests.csv'
+my $output_file = 'lat_tests.csv';
 
 my $iterations = 1000;
 
-@tests = ('ib_send_lat', 'ib_write_lat', 'ib_read_lat');
-@connection_types = ('RC', 'UC', 'UD');
-@options = ('', '-z', '-R');
+my @tests = ('ib_send_lat', 'ib_write_lat', 'ib_read_lat');
+my @connection_types = ('RC', 'UC', 'UD');
+my @options = ('', '-z', '-R');
 
 my $target;
 my $server;
@@ -20,7 +20,7 @@ if ( $mode =~ /server|target|slave/ ) {
   $target = '';
   $server = 1;
 }
-else if ( $mode =~ /client|initiator|master/ ) {
+elsif ( $mode =~ /client|initiator|master/ ) {
   $target = ' ' . shift(@ARGV) . ' -U';
   $server = 0;
 }
@@ -28,16 +28,41 @@ else {
   die "Invalid server/client specifier\n";
 }
 
-open(OUT, ">$output_file" . "_$size") || die "Could not open $output_file for writing\n";
+if ( !$server ) {
+  open(OUT, ">$output_file") || die "Could not open $output_file for writing\n";
+}
 
-for $test (@tests) {
-  for $option (@options) {
-    for $type (@connection_types) {
+for my $test (@tests) {
+  for my $option (@options) {
+    my $opt_string;
+    if ( $option eq '' ) {
+      $opt_string = 'IB Connection';
+    }
+    elsif ( $option eq '-z' ) {
+      $opt_string = 'RDMA CM Connection with IB QPs';
+    }
+    elsif ( $option eq '-R' ) {
+      $opt_string = 'RDMA CM QPs';
+    }
+    for my $type (@connection_types) {
       if ( !$server ) {
         sleep 1;
       }
       my $output = `$test -c $type $option -n $iterations -s $size $target`;
-      my @lines = split("\n", $output);
-      for (@lines) {
-        if ( /^([0-9]+), ([0-9.]+)$/ ) {
-          print OUT $2 . ',';
+      if ( !$server ) {
+        print OUT "$test ${size}B $type $opt_string,";
+        my @lines = split("\n", $output);
+        for (@lines) {
+          if ( /^([0-9]+), ([0-9.]+)$/ ) {
+            print OUT $2 . ',';
+          }
+        }
+        print OUT "\n";
+      }
+    }
+  }
+}
+
+if ( !$server ) {
+  close OUT;
+}
