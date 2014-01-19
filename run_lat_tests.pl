96112483d9e51b759718627f86b7c275b7ffa75a
ecarver <carver@ecarver.tk>
2014-01-19 14:53:30 -0500

    Removed all read and write tests and datagram connection types. Added multiple sizes

--- run_lat_tests.pl
+++ run_lat_tests.pl
@@ -4,13 +4,13 @@ use strict;
 use warnings;
 
 my $mode = shift || 'server';
-my $size = 2;
+my @sizes = (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096);
 my $output_file = 'lat_tests.csv';
 
 my $iterations = 1000;
 
-my @tests = ('ib_send_lat', 'ib_write_lat', 'ib_read_lat');
-my @connection_types = ('RC', 'UC', 'UD');
+my @tests = ('ib_send_lat');
+my @connection_types = ('RC', 'UC');
 my @options = ('', '-z', '-R');
 
 my $target;
@@ -32,32 +32,32 @@ if ( !$server ) {
   open(OUT, ">$output_file") || die "Could not open $output_file for writing\n";
 }
 
-for my $test (@tests) {
-  for my $option (@options) {
-    my $opt_string;
-    if ( $option eq '' ) {
-      $opt_string = 'IB Connection';
-    }
-    elsif ( $option eq '-z' ) {
-      $opt_string = 'RDMA CM Connection with IB QPs';
-    }
-    elsif ( $option eq '-R' ) {
-      $opt_string = 'RDMA CM QPs';
-    }
-    for my $type (@connection_types) {
-      if ( !$server ) {
-        sleep 1;
+for my $size (@sizes) {
+  for my $test (@tests) {
+    for my $option (@options) {
+      my $opt_string;
+      if ( $option eq '' ) {
+        $opt_string = 'IB Connection';
+      } elsif ( $option eq '-z' ) {
+        $opt_string = 'RDMA CM Connection with IB QPs';
+      } elsif ( $option eq '-R' ) {
+        $opt_string = 'RDMA CM QPs';
       }
-      my $output = `$test -c $type $option -n $iterations -s $size $target`;
-      if ( !$server ) {
-        print OUT "$test ${size}B $type $opt_string,";
-        my @lines = split("\n", $output);
-        for (@lines) {
-          if ( /^([0-9]+), ([0-9.]+)$/ ) {
-            print OUT $2 . ',';
-          }
+      for my $type (@connection_types) {
+        if ( !$server ) {
+          sleep 1;
         }
-        print OUT "\n";
+        my $output = `$test -c $type $option -n $iterations -s $size $target`;
+        if ( !$server ) {
+          print OUT "$test ${size}B $type $opt_string,";
+          my @lines = split("\n", $output);
+          for (@lines) {
+            if ( /^([0-9]+), ([0-9.]+)$/ ) {
+              print OUT $2 . ',';
+            }
+          }
+          print OUT "\n";
+        }
       }
     }
   }
